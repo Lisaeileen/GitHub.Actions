f057371c47eeb6234b6f4031c016c07b1a6e8027
lisaeileen <ucheodinukwe@yahoo.com>
2024-11-25 20:24:58 +0000

    This is a script to install git on OS

---
+++ gitinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "script to install git"
+echo "installation started"
+if [ "$(uname)" == "Linux" ];
+then
+        echo "this is linux box, installation git"
+	yum install git -y
+elif [ "$(uname)" == "Darwin" ];
+then
+	echo "this is not linux box"
+	echo "this is MacOS"
+	brew install git
+else
+	echo "not installing"
+fi
